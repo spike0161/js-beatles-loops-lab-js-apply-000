c75d1a8125cce428fd679e691598f0a23c3ec9c4
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-05-09 16:42:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ Function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
+
+
+
+
+// function johnLennonFacts() {
+//   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+//   var array = [];
+//   var i = 0;
+//   while (array.length < facts.length) {
+//     array.push(facts[i] + "!!!");
+//     i++;
+//   }
+//   return array;
+// }
+
+// function iLoveTheBeatles(number) {
+//   var array = [];
+//   do {
+//     array.push("I love the Beatles!");
+//     number += 1
+//   } while (number < 15);
+//   return array;
+// } 
